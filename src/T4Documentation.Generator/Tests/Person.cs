300ede04ff8782ce967677876abcd68dcfb1a5de
Jean-Michel <jean.gaud@24.com>
2016-04-28 16:05:17 +0200

    Added readme.MD and made some other improvements

---
+++ src/T4Documentation.Generator/Tests/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace T4Documentation.Generator.Tests
+{
+    /// <summary>
+    /// A programming structure that represents a person.
+    /// </summary>
+    /// <code>
+    /// var p = new Person();
+    /// </code>
+    public class Person
+    {
+        /// <summary>
+        /// This is the name of a person.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// This is the height of a person.
+        /// </summary>
+        public int Height { get; set; }
+
+        /// <summary>
+        /// Makes a person think
+        /// </summary>
+        /// <code>
+        /// var p = new Person();
+        /// p.Think();
+        /// </code>
+        public void Think()
+        {
+            //Do something
+        }
+
+        /// <summary>
+        /// Determine if a person is willing to dance
+        /// </summary>
+        /// <code>
+        /// var p = new Person();
+        /// var willingnessToDance = p.GetWillingnessToDance();
+        /// </code>
+        /// <returns></returns>
+        public bool GetWillingnessToDance()
+        {
+            return true;
+        }
+
+    }
+}
