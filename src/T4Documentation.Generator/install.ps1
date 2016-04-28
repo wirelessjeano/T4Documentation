ee3c4fe41632983d4e1a35f997ae9f2c284d6655
Jean-Michel <jean.gaud@24.com>
2016-04-28 13:03:47 +0200

    Added install powershell

---
+++ src/T4Documentation.Generator/install.ps1
@@ -0,0 +1,6 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$project.ProjectItems.Item("T4Documentation.tt").Properties.Item("BuildAction").Value = 0 #prjBuildActionNone
+$project.ProjectItems.Item("T4Documentation.tt.MarkDown.provider.t4").Properties.Item("BuildAction").Value = 0
+$project.ProjectItems.Item("T4Documentation.tt.MediaWiki.provider.t4").Properties.Item("BuildAction").Value = 0
+$project.ProjectItems.Item("T4Documentation.tt.settings.xml").Properties.Item("BuildAction").Value = 0
